59034085e6368ee2c0e41908d6e603f30f38c329
tony <tonyofbyteball@users.noreply.github.com>
2020-09-26 21:44:25 +0300

    start odex client when running idle

--- run-idle.js
+++ run-idle.js
@@ -3,7 +3,8 @@
 const odex = require('odex-client');
 const eventBus = require('ocore/event_bus');
 
-eventBus.on('headless_wallet_ready', () => {
+eventBus.on('headless_wallet_ready', async () => {
+	await odex.start();
 	console.log("\nPlease use the above pairing code to pair your Obyte wallet to this bot and manage it.\n");
 	console.log("Ctrl-C to exit");
 });
